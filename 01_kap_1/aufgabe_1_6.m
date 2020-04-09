0182d03ec1beb0ee7ee3856059bec12bdc806f30
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-04-09 15:26:33 +0200

    ableitungen in aufgabe_1_6 funktionieren jetzt

--- 01_kap_1/aufgabe_1_6.m
+++ 01_kap_1/aufgabe_1_6.m
@@ -3,24 +3,32 @@ L = 0.5;                                                                    % [m
 theta0 = 100;                                                               % [°C]
 k = 0.1;                                                                    % [h^-1]
 
-theta = @(x,t) theta0 .* exp(-k * t) .* sin((x * pi)./(L*2) + pi/4);
+% theta = @(x,t) theta0 .* exp(-k * t) .* sin((x * pi)./(L*2) + pi/4);
+syms t x
+theta = theta0 .* exp(-k * t) .* sin((x * pi)./(L*2) + pi/4);
+theta = symfun(theta,[x,t]);
+theta_grad = diff(theta(x,t));
+theta_grad = symfun(theta_grad,[x,t]);
 
-x = [0; L; L/2];
+% numeric
+x = [0 L L/2];
 t0 = 0;                                                                     % [s]
 t1 = 10;                                                                    % [h]
-t = [t0 t1];
+t = [t0; t1];
+[x,t]=meshgrid(x,t);
 
 %% a)
-theta1 = theta(x,t);
+theta1 = double(vpa(theta(x,t)));
 fprintf(1, ['a)\tTemperatur [°C] an den Stellen\n', ...
             '\t\t\t\tx=0\t\tx=L\t\tx=L/2\n',...
             '\tt0 = %.1fs\t%.3f\t%.3f\t%.3f\n'...
             '\tt1 = %.1fh\t%.3f\t%.3f\t%.3f\n'...
-            '\n'], [t; theta1]);
+            '\n'], [t(:,1), theta1]');
 
 %% b)
-theta_grad = @(theta,x,t) gradient(theta(x,t),x);
-thetagrad1 = theta_grad(theta,x,t1);
-fprintf(1,['b)\tTemperaturgradient [°C/m] nach t1 = %.1fh an den Stellen\n', ...
-    '\tx=0\t\tx=L\t\tx=L/2\n',...
-    '\t%.3f\t%.3f\t%.3f\n'], t1, thetagrad1);
+thetagrad1 = double(vpa(theta_grad(x,t)));
+fprintf(1,['b)\tTemperaturgradient [°C/m] an den Stellen\n', ...
+    '\t\t\t\tx=0\t\tx=L\t\tx=L/2\n',...
+    '\tt0 = %.1fs\t%.3f\t%.3f\t%.3f\n'...
+    '\tt1 = %.1fh\t%.3f\t%.3f\t%.3f\n'...
+    '\n'], [t(:,1), thetagrad1]');
