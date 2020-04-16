5ec993dff148da13774157b79baecddf9b3fb06f
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-04-16 23:41:24 +0200

    fixed filenaming errors

--- 01_kap_1/aufgabe_1_8.m
+++ 01_kap_1/aufgabe_1_8.m
@@ -1,27 +1,14 @@
-% global A B C D L delta
-A = 20;
-B = 1;
-C = 5e-3;
-D = 2;
-L = 4;
-delta = 0.4;
-x = linspace(0,10,1e3);
-y = linspace(0,0.4,1e3)';
+x = linspace(-10,10,1e3);
+y = x';
+[x,y] = meshgrid(x,y);
 
-calcw = @(x,y) A * (x./L).^D .* (y./delta).^B .* exp(-y./(x.*C));
+theta = 64./(x.^2 + y.^2 + 2);
+[gradt1,gradt2] = gradient(theta);
 
-wfeld = calcw(x,y);
-w_vec = calcw([0,1,2,3,4],y);
+ax1 = subplot(2,2,[1,2]);
+ax2 = subplot(2,2,3);
+ax3 = subplot(2,2,4);
 
-ax1 = subplot(2,1,1);
-ax2 = subplot(2,1,2);
-
-surf(x,y,wfeld,'EdgeColor','none','Parent',ax1)
-plot(y,w_vec,'Parent',ax2);
-legend('w @ 0m','w @ 1m','w @ 2m','w @ 3m','w @ 4m')
-
-% function w = calcw(x,y)
-% global A B C D L delta
-% w = A * (x./L).^D .* (y./delta).^B .* exp(-y./(x.*C));
-% 
-% end
+surf(x,y,theta,'EdgeColor','none','Parent',ax1)
+surf(x,y,gradt1,'EdgeColor','none','Parent',ax2)
+surf(x,y,gradt1,'EdgeColor','none','Parent',ax3)
