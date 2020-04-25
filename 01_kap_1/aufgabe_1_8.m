2ac61912a89dc2ba0e17e8e3bca74313cfcce4cc
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-04-25 19:05:54 +0200

    implemented a symbolic math approach implemented 3d plots for both the temperature field and the directional derivative

--- 01_kap_1/aufgabe_1_8.m
+++ 01_kap_1/aufgabe_1_8.m
@@ -1,24 +1,44 @@
-P = [1; 1];
+range = [-1 1 -1 1] * 3;
+
+P = [1; 2];
 alpha = deg2rad(60);
 unitvec = [cos(alpha);sin(alpha)];
 
+%% calculation
 syms x y;
 theta = symfun(64/(x.^2 + y.^2 + 2),[x,y]);
-fsurf(theta,[-5 5 -5 5]);
-quiver(P',unitvec')
-return;
 
-x = linspace(-10,10,1e3);
-y = x';
-[x,y] = meshgrid(x,y);
+% richtungsableitungen
+theta_gradx = symfun(diff(theta(x,y),x),[x,y]);
+theta_grady = symfun(diff(theta(x,y),y),[x,y]);
 
-theta = 64./(x.^2 + y.^2 + 2);
-[gradt1,gradt2] = gradient(theta);
+% numeric
+x = P(1);
+y = P(2);
+grad_theta = double(vpa([theta_gradx(x,y);theta_grady(x,y)]));
 
-ax1 = subplot(2,2,[1,2]);
-ax2 = subplot(2,2,3);
-ax3 = subplot(2,2,4);
+fprintf('theta ändert sich an P(%i|%i) mit %.4f °C/cm.\n',P,sum(grad_theta .* unitvec))
 
-surf(x,y,theta,'EdgeColor','none','Parent',ax1)
-surf(x,y,gradt1,'EdgeColor','none','Parent',ax2)
-surf(x,y,gradt1,'EdgeColor','none','Parent',ax3)
+%% plotting
+% 3d field
+axH(1) = subplot(2,2,1);
+fsurf(theta,range);
+
+% 2d field with vector
+axH(2) = subplot(2,2,2);
+fcH = fcontour(theta,range,'Fill','on','LevelStep',.1);
+axH(2).NextPlot = 'add';
+quivH = quiver(P(1),P(2),unitvec(1),unitvec(2),'Color','r');
+axH(2).NextPlot = 'replace';
+
+% theta grad x
+axH(3) = subplot(2,2,3);
+fsurf(theta_gradx,range);
+
+% theta grad y
+axH(4) = subplot(2,2,4);
+fsurf(theta_grady,range);
+
+% link plots
+hLink = linkprop([axH([1,3,4])],{'CameraPosition'});
+rotate3d on;
