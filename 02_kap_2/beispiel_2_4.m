6a3091ef631a09f565893abf87dd82fd222e263b
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-04-28 16:27:51 +0200

    improved readibility

--- 02_kap_2/beispiel_2_4.m
+++ 02_kap_2/beispiel_2_4.m
@@ -6,16 +6,30 @@ k = 1;                  % [W * m^-2 * K^-1]
 A = 4;                  % [m^2]
 rho_L = 1.2;            % [kg * m^-3]
 
+% notable temperatures
 theta0 = 50;            % [°C]
 thetaInf = 25;          % [°C]
+thetaG = 28;            % [°C]
 
-beta = Vdot/V + (k * A) / (rho_L * V * cp);
+% ODE prefactor
+betafun = @(Vdot,V,k,A,rho_l,cp) Vdot/V + (k * A) / (rho_L * V * cp);
 
+% ode setup
 tspan = [0 1e3];
+beta = betafun(Vdot,V,k,A,rho_L,cp);
 
+%% d) Grenztemperatur
 [t,theta] = ode45(@(t,theta) odefun(t,theta,beta,thetaInf),tspan,theta0);
 
-plot(t,theta)
+figH = getFigH(1,'Color','default','WindowSytle','docked');
+axH = axes(figH); grid on; grid minor;
+
+thetaPlt = line(axH,t,theta);
+thetaGPlt = yline(axH,thetaG,'LineStyle','--');
+
+
+
+clearvars axH figH thetaPlt thetaGPlt
 
 function dydt = odefun(t,theta,beta,thetaInf)
 dydt = -beta * (theta - thetaInf);
